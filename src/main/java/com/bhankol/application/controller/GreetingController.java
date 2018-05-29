2be2ff439b4e9dd0b089d4d45aa6a11ef08b18d1
Pravin Gosavi <pravin.gosavi@qiotec.com>
2018-05-29 10:09:30 +0530

    Added a Greeting Controller

---
+++ src/main/java/com/bhankol/application/controller/GreetingController.java
@@ -0,0 +1,17 @@
+package com.bhankol.application.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by pravingosavi on 29/05/18.
+ */
+public class GreetingController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    @ResponseBody
+    public String sayHello() {
+        return "Secure Hello!";
+    }
+}
