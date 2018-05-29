0223e96bd329490b2ac0bcc0cd67da8494d3e3b4
Pravin Gosavi <pravin.gosavi@qiotec.com>
2018-05-29 21:40:31 +0530

    Added Properties files for diffrent environments

--- src/main/java/com/bhankol/application/controller/GreetingController.java
+++ src/main/java/com/bhankol/application/controller/GreetingController.java
@@ -1,17 +1,14 @@
 package com.bhankol.application.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by pravingosavi on 29/05/18.
  */
+
+@RestController
 public class GreetingController {
 
-    @RequestMapping(method = RequestMethod.GET)
-    @ResponseBody
-    public String sayHello() {
-        return "Secure World!";
-    }
+
 }
