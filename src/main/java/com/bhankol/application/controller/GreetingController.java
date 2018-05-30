01f293ed781cbb7d75d787b5760eb92fd2417dec
Pravin Gosavi <pravin.gosavi@qiotec.com>
2018-05-30 18:02:24 +0530

    Added Stage for Staging Environment in Jenkinsfile

--- src/main/java/com/bhankol/application/controller/GreetingController.java
+++ src/main/java/com/bhankol/application/controller/GreetingController.java
@@ -1,14 +1,26 @@
 package com.bhankol.application.controller;
 
 
+import com.bhankol.application.entity.User;
+import com.bhankol.application.service.UserService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 /**
  * Created by pravingosavi on 29/05/18.
  */
 
 @RestController
 public class GreetingController {
-
 
 
 }
